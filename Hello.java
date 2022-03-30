76f655895316a2726052a9bda423fa0f9a700dd5
ckqn06 <ckqn06@naver.com>
2022-03-30 16:28:24 +0900

    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		AddOne ao = new AddOne();
+		int y = ao.addOne(10);
+		
+		System.out.println("y = " + y);
+		
+		/*
+		for (int i=0)
+		 */
+	}
+}
